74efc63e1209d3bb2b5422d4fa8cb0cc26d18563
Jon Schroeder <jon@elod.in>
2021-01-08 15:46:02 -0600

    Adding colors

--- lib/customize-colors.php
+++ lib/customize-colors.php
@@ -28,7 +28,7 @@ function urban_color_controls_customizer( $wp_customize ) {
 	$wp_customize->add_setting(
 		'secondary_color',
 		array(
-			'default'           => '#333333',
+			'default'           => '#333331',
 			'sanitize_callback' => 'sanitize_hex_color',
 		)
 	);
@@ -91,7 +91,7 @@ function urban_color_controls_customizer( $wp_customize ) {
 	$wp_customize->add_setting(
 		'default_button_link',
 		array(
-			'default'           => '#333333',
+			'default'           => '#333330',
 			'sanitize_callback' => 'sanitize_hex_color',
 		)
 	);
@@ -105,6 +105,48 @@ function urban_color_controls_customizer( $wp_customize ) {
 				'label'       => __( 'Buttons & Links', 'brindle_urban' ),
 				'section'     => 'colors',
 				'settings'    => 'default_button_link',
+			)
+		)
+	);
+	
+	$wp_customize->add_setting(
+		'bonus_color_1',
+		array(
+			'default'           => '#333334',
+			'sanitize_callback' => 'sanitize_hex_color',
+		)
+	);
+	
+	$wp_customize->add_control(
+		new WP_Customize_Color_Control(
+			$wp_customize,
+			'bonus_color_1',
+			array(
+				'description' => __( 'A bonus color. This will be enabled in the Gutenberg editor but will not automatically change any theme colors.', 'brindle_urban' ),
+				'label'       => __( 'Bonus color 1', 'brindle_urban' ),
+				'section'     => 'colors',
+				'settings'    => 'bonus_color_1',
+			)
+		)
+	);
+	
+	$wp_customize->add_setting(
+		'bonus_color_2',
+		array(
+			'default'           => '#333335',
+			'sanitize_callback' => 'sanitize_hex_color',
+		)
+	);
+	
+	$wp_customize->add_control(
+		new WP_Customize_Color_Control(
+			$wp_customize,
+			'bonus_color_2',
+			array(
+				'description' => __( 'Another bonus color. This will be enabled in the Gutenberg editor but will not automatically change any theme colors.', 'brindle_urban' ),
+				'label'       => __( 'Bonus color 2', 'brindle_urban' ),
+				'section'     => 'colors',
+				'settings'    => 'bonus_color_2',
 			)
 		)
 	);
@@ -140,6 +182,14 @@ function urban_output_color_vars() {
 		array(
 			'label' => 'link',
 			'hex' => get_theme_mod( 'default_button_link' ),
+		),
+		array(
+			'label' => 'bonus_color_1',
+			'hex' => get_theme_mod( 'bonus_color_1' ),
+		),
+		array(
+			'label' => 'bonus_color_2',
+			'hex' => get_theme_mod( 'bonus_color_2' ),
 		),
 	);
 		
@@ -179,6 +229,8 @@ function urban_register_colors() {
 	$light			 	= get_theme_mod( 'light' );
 	$default		 	= get_theme_mod( 'default_text' );
 	$link		 		= get_theme_mod( 'default_button_link' );
+	$bonus_color_1		= get_theme_mod( 'bonus_color_1' );
+	$bonus_color_2		= get_theme_mod( 'bonus_color_2' );
 	
 	add_theme_support(
 		'editor-color-palette', array(
@@ -186,16 +238,6 @@ function urban_register_colors() {
 				'name'  => esc_html__( 'White', 'brindle_urban' ),
 				'slug' => 'white',
 				'color' => '#ffffff',
-			),
-			array(
-				'name'  => esc_html__( 'Primary', 'brindle_urban' ),
-				'slug' => 'primary',
-				'color' => $primary_color,
-			),
-			array(
-				'name'  => esc_html__( 'Secondary', 'brindle_urban' ),
-				'slug' => 'secondary',
-				'color' => $secondary_color,
 			),
 			array(
 				'name'  => esc_html__( 'Light', 'brindle_urban' ),
@@ -208,9 +250,29 @@ function urban_register_colors() {
 				'color' => $default,
 			),
 			array(
+				'name'  => esc_html__( 'Primary', 'brindle_urban' ),
+				'slug' => 'primary',
+				'color' => $primary_color,
+			),
+			array(
+				'name'  => esc_html__( 'Secondary', 'brindle_urban' ),
+				'slug' => 'secondary',
+				'color' => $secondary_color,
+			),			
+			array(
 				'name'  => esc_html__( 'link', 'brindle_urban' ),
 				'slug' => 'link',
 				'color' => $link,
+			),
+			array(
+				'name'  => esc_html__( 'bonus_color_1', 'brindle_urban' ),
+				'slug' => 'bonus_color_1',
+				'color' => $bonus_color_1,
+			),
+			array(
+				'name'  => esc_html__( 'bonus_color_2', 'brindle_urban' ),
+				'slug' => 'bonus_color_2',
+				'color' => $bonus_color_2,
             ),
 		)
 	);
