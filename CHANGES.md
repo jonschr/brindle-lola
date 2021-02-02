c2fbb9c857a23932390508a091d94402758fdf81
Jon Schroeder <jon@elod.in>
2021-02-01 23:09:43 -0600

    Fixing hiding .expand functionality

--- CHANGES.md
+++ CHANGES.md
@@ -1,2 +1,5 @@
+### 1.2.2
+* Bugfix: Hidden lists when not intended to be hidden
+
 ### 1.2.1 
 * Bugfix: Submenus were hiding their last child
--- js/expand.js
+++ js/expand.js
@@ -1,6 +1,7 @@
 jQuery(document).ready(function ($) {
 
-    // $('.expand').siblings('ul').children().hide();
+    $('.expand').siblings('ul').children().hide();
+    $('.expand').siblings('ul').children(':nth-of-type(1),:nth-of-type(2),:nth-of-type(3),:nth-of-type(4)').show();
 
     $('.expand').on('click', expandnearest);
 
